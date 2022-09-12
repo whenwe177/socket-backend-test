4a23e8aef4daefb092f696eaabdb59557debde90
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-09-12 09:12:33 +0700

    Add new project

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const io = require("socket.io")(process.env.PORT, {
+  cors: {
+    origin: ["http://localhost:3000", "http://socket-test-cyclic.vercel.app"]
+  }  
+});
+
+io.on("connection", socket => {
+  console.log(`Socket with id ${socket.id} is connected.`);
+
+  socket.on("send-message", message => {
+    console.log("Chat sent")
+    socket.broadcast.emit("receive-message", message);
+  });
+
+  socket.on("end-connection", () => {
+    socket.disconnect
+  })
+});
+
+
+app.listen(8000, () => console.log("Server listening on port 8000"));
